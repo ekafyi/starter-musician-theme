0c7306b8414686aecdce8018a75074c29cb27b47
Eka <ekafyi@users.noreply.github.com>
2019-08-05 22:03:52 +0700

    Initial commit - add packages and gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    // Add gatsby-theme-musician to your site
+    {
+      resolve: "gatsby-theme-musician",
+      // More info about the theme options:
+      // https://github.com/ekafyi/gatsby-theme-musician/blob/master/docs/usage.md#theme-options
+      options: {
+        // These are default values that you can override
+        // basePath: `/`,
+        // content: `content`,
+        // pwaName: null
+      }
+    }
+  ]
+};
